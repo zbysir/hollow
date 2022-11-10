d8929007b189d7beb222e6f6f950b4ce62a607e2
zhangliang <zbysir@qq.com>
2022-11-10 14:12:57 +0800

    doing compass theme

--- docs/theme/compass/component/Link.tsx
+++
@@ -1,9 +0,0 @@
-import hollow from "@bysir/hollow"
-
-let params = hollow.getConfig();
-
-export default function Link(props) {
-    let base = params?.base || ''
-
-    return <a {...props} href={base + props.href}>{props.children}</a>
-}
